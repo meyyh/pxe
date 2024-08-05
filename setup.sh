78d5ee10bbc5b251eec608f39ff19fbe2b0354df
Cooper Sanders <94275329+meyyh@users.noreply.github.com>
2024-08-05 18:20:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,59 +9,52 @@ fi
 WAN=""
 LAN=""
 DNS_SERVER=$(cat /etc/resolv.conf | grep nameserver | cut -d " " -f 2)
+PXE_DIR=/pxe
 
-#get the wan and lan interface names
-function interfaces()
-{
-    # Display network interfaces with colors
-    ip --color address
-    INTERFACES=$(ip -o link show | awk -F': ' '{print $2}')
 
-    while true; do
-        echo "Enter the name of the WAN interface:"
-        read TEMP_WAN
-        if echo "$INTERFACES" | grep -qw "$TEMP_WAN"; then
-            # Valid WAN interface found
-            break
-        else
-            echo "Not a valid interface. Please try again."
-        fi
-    done
+# Display network interfaces with colors
+ip --color address
+INTERFACES=$(ip -o link show | awk -F': ' '{print $2}')
 
-    while true; do
-        echo "Enter the name of the LAN interface:"
-        read TEMP_LAN
-        if echo "$INTERFACES" | grep -qw "$TEMP_LAN"; then
-            # Valid LAN interface found
-            break
-        else
-            echo "Not a valid interface. Please try again."
-        fi
-    done
-
-    if [ "$TEMP_WAN" = "$TEMP_LAN" ]; then
-        echo "WAN and LAN interfaces can't be the same. Please try again."
-        interfaces
+while true; do
+    echo "Enter the name of the WAN interface:"
+    read TEMP_WAN
+    if echo "$INTERFACES" | grep -qw "$TEMP_WAN"; then
+        # Valid WAN interface found
+        break
+    else
+        echo "Not a valid interface. Please try again."
     fi
+done
 
-    WAN=$TEMP_WAN
-    LAN=$TEMP_LAN
-}
+while true; do
+    echo "Enter the name of the LAN interface:"
+    read TEMP_LAN
+    if echo "$INTERFACES" | grep -qw "$TEMP_LAN"; then
+        # Valid LAN interface found
+        break
+    else
+        echo "Not a valid interface. Please try again."
+    fi
+done
 
-interfaces
+if [ "$TEMP_WAN" = "$TEMP_LAN" ]; then
+    echo "WAN and LAN interfaces can't be the same. Please try again."
+    exit 1
+fi
 
+WAN=$TEMP_WAN
+LAN=$TEMP_LAN
+
+ip addr flush dev $LAN
 ip addr add 123.123.0.1/16 dev $LAN
 
-read -p "Delete /pxe-boot : " -n 1 -r
+read -p "Delete $PXE_DIR : " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    rm -rf /pxe-boot
+    rm -rf $PXE_DIR
 fi
-
-mkdir -p /pxe-boot/menu
-mkdir -p /pxe-boot/os/win
-mkdir /pxe-boot/os/linux
 
 apt-get install ipxe dnsmasq nginx git iptables wget -y
 
@@ -74,27 +67,20 @@ else
     exit 1
 fi
 
-if [ -d "pxe" ]; then
-    cd pxe
-    git pull
-    cd ..
-else
-    git clone https://github.com/meyyh/pxe
-fi
+mkdir    $PXE_DIR
+mkdir    $PXE_DIR/menu
+mkdir -p $PXE_DIR/os/win
+mkdir    $PXE_DIR/os/linux
 
-cp ./pxe/menu.ipxe /pxe-boot/menu/
-
+cp ./pxe/menu.ipxe $PXE_DIR/menu/
+cp /usr/lib/ipxe/undionly.kpxe $PXE_DIR
+cp /usr/lib/ipxe/ipxe.efi $PXE_DIR
 cp /etc/dnsmasq.conf /etc/dnsmasq.conf.old
-
 cat ./pxe/dnsmasq.conf > /etc/dnsmasq.conf
 
 #replace values in dnsmasq.conf
 sed -i "s/_rep-interface/$LAN/" /etc/dnsmasq.conf
 sed -i "s/_rep-dns-server/$DNS_SERVER/" /etc/dnsmasq.conf
-
-
-cp /usr/lib/ipxe/undionly.kpxe /pxe-boot
-cp /usr/lib/ipxe/ipxe.efi /pxe-boot
 
 #setup routing from lan to wan and back
 # enable ip forwarding in the kernel
@@ -114,7 +100,6 @@ iptables -A FORWARD -i $WAN -o $LAN -m state --state NEW,RELATED,ESTABLISHED -j 
 iptables -A FORWARD -i $LAN -o $WAN -j ACCEPT
 
 iptables -A INPUT -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
-
 
 #start dnsmasq
 systemctl enable dnsmasq && systemctl start dnsmasq
